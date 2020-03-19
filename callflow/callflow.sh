daf422615cbb60b980264ae69803ea667987218a
Doomguy <1833568+doomguy@users.noreply.github.com>
2020-03-19 18:21:22 +0100

    Create callflow.sh

---
+++ callflow/callflow.sh
@@ -0,0 +1,6 @@
+DOMAIN="$1"
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+
+echo "$DOMAIN"
+#curl -sLv -A $USER_AGENT $DOMAIN 2>&1 | grep -Ei '(^> (GET|Host)|Location:)' | grep -v '^*' | sed 's,^<,  <,g'
+wget -O- -U "$USER_AGENT" "$DOMAIN" 2>&1 | grep -i '^Location:'
