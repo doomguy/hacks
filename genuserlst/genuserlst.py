23ca0693121f174d62a601be3a71482d2bc8c385
Doomguy <1833568+doomguy@users.noreply.github.com>
2020-06-11 00:00:09 +0200

    Update genuserlst.py

--- genuserlst/genuserlst.py
+++ genuserlst/genuserlst.py
@@ -7,10 +7,10 @@ def main():
    
     if len(sys.argv)<2:
       print("Usage: {} user.txt | sort -u > mylist.txt".format(sys.argv[0]))
-      print("Script expects list of usersnames in the following format:")
+      print("Script can handle entries of usersnames in the following formats:")
       print("  John Doe")
-      print("  Jane Doe")
-      print("  Peter Parker")
+      print("  Admin")
+      print("  PeterParker:Spiderm4n")
       sys.exit()
 
     filepath = sys.argv[1]
@@ -43,20 +43,33 @@ def main():
         print("{}.{}".format(nlist[0],nlist[1]))  # f.l
         print("{}.{}".format(nlist[1],nlist[0]))  # l.f
            
+        print("{}_{}".format(nlist[0],nlist[1]))  # f_l
+        print("{}_{}".format(nlist[1],nlist[0]))  # l_f
+           
         print("{}{}".format(nlist[0][0],nlist[1]))  # 1f+l
         print("{}{}".format(nlist[1][0],nlist[0]))  # 1l+f
            
         print("{}.{}".format(nlist[0][0],nlist[1]))  # 1f.l
         print("{}.{}".format(nlist[1][0],nlist[0]))  # 1l.f
 
+        print("{}_{}".format(nlist[0][0],nlist[1]))  # 1f_l
+        print("{}_{}".format(nlist[1][0],nlist[0]))  # 1l_f
+
         print("{}.{}".format(nlist[0],nlist[1][0]))  # f.1l
         print("{}.{}".format(nlist[1],nlist[0][0]))  # l.1f
+
+        print("{}_{}".format(nlist[0],nlist[1][0]))  # f_1l
+        print("{}_{}".format(nlist[1],nlist[0][0]))  # l_1f
 
         print("{}.{}".format(nlist[0][0],nlist[1][0]))  # 1f.1l
         print("{}.{}".format(nlist[1][0],nlist[0][0]))  # 1l.1f
 
+        print("{}_{}".format(nlist[0][0],nlist[1][0]))  # 1f_1l
+        print("{}_{}".format(nlist[1][0],nlist[0][0]))  # 1l_1f
+
         print("{}.{}.".format(nlist[0][0],nlist[1][0]))  # 1f.1l.
         print("{}.{}.".format(nlist[1][0],nlist[0][0]))  # 1l.1f.
+
 
 if __name__ == '__main__':
     main()
