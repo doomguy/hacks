92a5b14456098c40f0c3c32341f38a9dd6429440
Doomguy <1833568+doomguy@users.noreply.github.com>
2020-06-10 23:38:36 +0200

    Create genuserlst.py

---
+++ genuserlst/genuserlst.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+def main():
+   
+    if len(sys.argv)<2:
+      print("Usage: {} user.txt | sort -u > mylist.txt".format(sys.argv[0]))
+      print("Script expects list of usersnames in the following format:")
+      print("  John Doe")
+      print("  Jane Doe")
+      print("  Peter Parker")
+      sys.exit()
+
+    filepath = sys.argv[1]
+
+    if not os.path.isfile(filepath):
+      print("File {} does not exist. Exiting...".format(filepath))
+      sys.exit()
+  
+    with open(filepath) as fp:
+      for input in fp:
+        line=input.strip()
+        #print(line) # Debug
+        userpart=line.split(':') # Support for combolists 'johnDoe:s3cr3tp4$$w0rd"
+        #print(userpart) # Debug
+        nlist=userpart[0].split(' ')
+        #print(nlist) # Debug
+
+        print(userpart[0])      # Unmodified
+
+        # If only one word is given, just print and continue with next entry
+        if len(nlist)<2:
+          continue 
+
+        print("{} {}".format(nlist[1],nlist[0]))  # l f
+        print(nlist[0])  # f
+        print(nlist[1])  # l
+        print("{}{}".format(nlist[0],nlist[1]))  # f+l
+        print("{}{}".format(nlist[1],nlist[0]))  # l+f
+          
+        print("{}.{}".format(nlist[0],nlist[1]))  # f.l
+        print("{}.{}".format(nlist[1],nlist[0]))  # l.f
+           
+        print("{}{}".format(nlist[0][0],nlist[1]))  # 1f+l
+        print("{}{}".format(nlist[1][0],nlist[0]))  # 1l+f
+           
+        print("{}.{}".format(nlist[0][0],nlist[1]))  # 1f.l
+        print("{}.{}".format(nlist[1][0],nlist[0]))  # 1l.f
+
+        print("{}.{}".format(nlist[0],nlist[1][0]))  # f.1l
+        print("{}.{}".format(nlist[1],nlist[0][0]))  # l.1f
+
+        print("{}.{}".format(nlist[0][0],nlist[1][0]))  # 1f.1l
+        print("{}.{}".format(nlist[1][0],nlist[0][0]))  # 1l.1f
+
+        print("{}.{}.".format(nlist[0][0],nlist[1][0]))  # 1f.1l.
+        print("{}.{}.".format(nlist[1][0],nlist[0][0]))  # 1l.1f.
+
+if __name__ == '__main__':
+    main()
