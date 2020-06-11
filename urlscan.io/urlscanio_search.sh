387e5f1743737838be35454539e2f44630fa986f
Doomguy <1833568+doomguy@users.noreply.github.com>
2020-06-12 01:40:24 +0200

    Create urlscanio_search.sh

---
+++ urlscan.io/urlscanio_search.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Remeber to use: https://www.shellcheck.net
+set -euo pipefail
+IFS=$'\n\t'
+
+# Uncomment for Debugging
+#set -x
+
+# Search urlscan.io for a domain and check the http response code; filters pages with status 404
+
+print_usage() {
+  echo "Usage:"
+  echo "\$ $0 example.com | tee example_com.log"
+  exit
+}
+
+main() {
+  curl -s "https://urlscan.io/api/v1/search/?q=$DOMAIN" \
+   | jq '.results|.[].task.url' \
+   | grep -iE "$DOMAIN/" \
+   | xargs -I§ curl -s -o /dev/null -w "%{http_code}:%{url_effective}\n" § \
+   | grep -v ^404
+}
+
+if [ $# = 1 ]; then
+  if [ "$1" = "-h" ]; then
+    print_usage
+  else
+    DOMAIN="$1"
+    main
+  fi
+  exit
+else
+  print_usage
+fi
