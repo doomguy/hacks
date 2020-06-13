25ca9a729bc3aeefa66215b86d7d0d67e9fc2c08
Doomguy <1833568+doomguy@users.noreply.github.com>
2020-06-13 10:25:32 +0200

    Create httpstat.sh

---
+++ httpstat/httpstat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Remeber to use: https://www.shellcheck.net
+set -euo pipefail
+IFS=$'\n\t'
+
+# Uncomment for Debugging
+#set -x
+
+# Script to return status code of given urls
+
+# 2DO:
+# - Implement proper user agent handling
+
+while IFS= read -r domain; do
+  curl -k -s -o /dev/null -w "%{http_code}: %{url_effective}\n" --connect-timeout 10 "$domain"
+done
