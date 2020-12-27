b620f517fc72289e3b22547caf0e98f838a848cd
Doomguy <Doomguy>
2020-12-27 22:22:07 +0100

    small improvements for output

--- update.sh/update.sh
+++ update.sh/update.sh
@@ -11,7 +11,7 @@ export LC_ALL=C
 
 # Debian/Ubuntu
 if [ -f "$(which apt)" ] && [ -x "$(which apt)" ]; then
-    echo "[*] Starting updates using 'apt'"
+    echo -e "\n[*] Starting updates using 'apt'"
     apt update &&
     apt dist-upgrade -y
     #apt autoremove -y
@@ -19,22 +19,22 @@ fi
 
 # CentOS/Fedora
 if [ -f "$(which dnf)" ] && [ -x "$(which dnf)" ]; then
-    echo "[*] Starting updates using 'dnf'"
+    echo -e "\n[*] Starting updates using 'dnf'"
     dnf update
 elif [ -f "$(which yum)" ] && [ -x "$(which yum)" ]; then
-    echo "[*] Starting updates using 'yum'"
+    echo -e "\n[*] Starting updates using 'yum'"
     yum update
 fi
 
 # Arch/Manjaro
 if [ -f "$(which pacman)" ] && [ -x "$(which pacman)" ]; then
-    echo "[*] Starting updates using 'pacman'"
+    echo -e "\n[*] Starting updates using 'pacman'"
     pacman -Syu
 fi
 
 # macOS Homebrew
 if [ -f "$(which brew)" ] && [ -x "$(which brew)" ]; then
-    echo "[*] Starting updates using 'brew'"
+    echo -e "\n[*] Starting updates using 'brew'"
     brew update &&
     brew upgrade &&
     brew cleanup -s &&
@@ -45,7 +45,7 @@ fi
 
 # Upgrade atom
 if [ -f "$(which apm)" ] && [ -x "$(which apm)" ]; then
-    echo "[*] Starting atom updates using 'apm'"
+    echo -e "\n[*] Starting atom updates using 'apm'"
     apm upgrade -c false
 fi
 
@@ -55,12 +55,14 @@ fi
 
 # ruby gems
 if [ -f "$(which gem)" ] && [ -x "$(which gem)" ]; then
-    echo "[*] Starting ruby gem updates using 'gem'"
+    echo -e "\n[*] Starting ruby gem updates using 'gem'"
     gem update
 fi
 
 # npm upgrade
 if [ -f "$(which npm)" ] && [ -x "$(which npm)" ]; then
-    echo "[*] Starting npm updates using 'npm'"
+    echo -e "\n[*] Starting npm updates using 'npm'"
     npm update -g
 fi
+
+echo -e "\n[*] All done! Exiting.."
