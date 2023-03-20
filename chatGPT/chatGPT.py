4c7936fa7eb5dc43039b1c713cf35722dea85aac
Doomguy <1833568+doomguy@users.noreply.github.com>
2023-03-20 01:05:48 +0100

    Add files via upload

--- chatGPT/chatGPT.py
+++ chatGPT/chatGPT.py
@@ -6,6 +6,9 @@ import sys
 import os
 from prompt_toolkit import PromptSession
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.completion import WordCompleter
 
 parser = argparse.ArgumentParser(description="Query ChatGPT using the API")
 parser.add_argument("--key", type=str, help="OpenAI API key")
@@ -28,28 +31,60 @@ def cancel_(event):
 
 
 def main(args):
+    # check for API key
     if not openai.api_key and not args.key:
         parser.error("--key or OPENAI_API_KEY environment variable is required.")
     elif not openai.api_key:
         openai.api_key = args.key
 
-    session = PromptSession()
+    # prompt history
+    history_file = os.path.join(os.path.expanduser("~"), ".chatgpt_history")
+    # Check if file ~/.chatgpt_history exists. If not, create it.
+    if not os.path.exists(history_file):
+        with open(history_file.split("/")[-1], "w") as f:
+            f.write("")
 
+    # setup prompt session
+    session = PromptSession(
+        history=FileHistory(
+            history_file,
+        )
+    )
+
+    # setup word completer
+    cmds_exit = ["exit", "quit", "q", "logout", "bye"]
+    cmds_clear = ["clear", "cls"]
+    cmds_all = cmds_exit + cmds_clear
+
+    # prompt loop
     while True:
-        text = session.prompt("ChatGPT> ", key_bindings=kb)
-        if text.strip() == "":
+
+        commands = WordCompleter(cmds_all, ignore_case=True)
+        prompt = session.prompt(
+            "Prompt> ",
+            key_bindings=kb,
+            completer=commands,
+            auto_suggest=AutoSuggestFromHistory(),
+            multiline=True,
+        )
+        if prompt.strip() == "":
             continue
-        elif text.strip() in ["quit", "bye", "exit", "logout"]:
+        elif prompt.strip().lower() in cmds_exit:
             sys.exit(1)
-        elif text.strip() == "clear":
+        elif prompt.strip() in cmds_clear:
             os.system("clear")
         else:
-            query_chatgpt(text)
+            try:
+                query_chatgpt(prompt)
+            except Exception as e:
+                print(f"Error:{e}")
+
+        print("\n<<<>>>\n")  # prompt separator
 
 
-def query_chatgpt(query):
-    prompt = query
-    response = openai.Completion.create(
+def query_chatgpt(prompt):
+
+    """response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
         temperature=0.5,
@@ -58,10 +93,23 @@ def query_chatgpt(query):
         frequency_penalty=0.0,
         presence_penalty=0.0,
         stream=False,
+    )"""
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        frequency_penalty=0.0,  # avoid repeating the same words or phrases, default: 0
+        presence_penalty=0.0,  # avoid using words or phrases from prompt or context, default: 0
+        stream=False,  # return a continuous stream of responses, default: False
+        temperature=0.5,  # control the creativity of the generated text, recommended: 0.7-1.0
+        top_p=0.7,  # control the diversity of responses, recommened: 0.7-0.9
     )
 
-    response = response.choices[0]["text"].strip()
-    print(f"\n{response}\n")
+    completion = completion.choices[0]["message"]["content"].strip()
+    print(f"\n{completion}")
 
 
 if __name__ == "__main__":
